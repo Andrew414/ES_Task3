1866c1697b380c6167e34272a5b573c3babff09a
AnastasiyaKoloskova <anastasiya.koloskova@gmail.com>
2013-03-26 12:22:26 +0300

    Update server.c
    new commit

--- server.c
+++ server.c
@@ -20,43 +20,42 @@ int main(int argc, char *argv[])
   if(argc != 2) {
 		printf("Enter the command line parameter with file name.\n");
 		return 1;
-	} else {
-		struct file_content info = read_file(argv[1]); 
-		printf("%s\n", info.content);
-		// Defining and initializing socket
-		int sock = 0, connfd = 0;
-		socketaddress_in server_address; 
-
-		sock = socket(AF_INET, SOCK_STREAM, 0);
-		memset(&server_address, '0', sizeof(server_address));
-
-		server_address.sin_family = AF_INET;
-		server_address.sin_addr.s_addr = htonl(INADDR_ANY);
-		server_address.sin_port = htons(5000); 
-
-		// Bind socket to address
-		bind(sock, (struct sockaddr*)&server_address, sizeof(server_address)); 
-
-		int i = 5;
-		// Starting server
-		while(i--) {
-			// Start listening socket
-			listen(sock, 10);
-
-			//Initializing data for starting sending file in the separate thread
-			my_message mes;
-			mes.file = info.content;
-			mes.size = info.size;
-
-			// Accepting connection and getting connection identifier
-			mes.index = accept(sock, (struct sockaddr*)NULL, NULL); 
-			pthread_t thread;
-			pthread_create(&thread, NULL, send_file_to_client, (void*)&mes);
-		}
-
-
-		close(sock);
-		free(info.content);
-		return 0;
 	}
+	file_content info = read_file(argv[1]); 
+	printf("%s\n", info.content);
+	// Defining and initializing socket
+	int sock = 0, connfd = 0;
+	socketaddress_in server_address; 
+
+	sock = socket(AF_INET, SOCK_STREAM, 0);
+	memset(&server_address, 0, sizeof(server_address));
+
+	server_address.sin_family = AF_INET;
+	server_address.sin_addr.s_addr = htonl(INADDR_ANY);
+	server_address.sin_port = htons(5000); 
+
+	// Bind socket to address
+	bind(sock, (SOCKET_ADDRESS*)&server_address, sizeof(server_address)); 
+
+	int i = 5;
+	// Starting server
+	while(i--) {
+		// Start listening socket
+		listen(sock, 10);
+
+		//Initializing data for starting sending file in the separate thread
+		my_message mes;
+		mes.file = info.content;
+		mes.size = info.size;
+
+		// Accepting connection and getting connection identifier
+		mes.index = accept(sock, (SOCKET_ADDRESS*)NULL, NULL); 
+		pthread_t thread;
+		pthread_create(&thread, NULL, send_file_to_client, (void*)&mes);
+	}
+
+
+	close(sock);
+	free(info.content);
+	return 0;
 }
