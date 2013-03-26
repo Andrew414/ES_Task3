b82c8041b623d60e82bc8c31034e5b0c0676c71f
Andrew414 <andrew414414@gmail.com>
2013-03-26 07:56:13 -0700

    Update server_filereader.c
    server_filereader (commit from web)

---
+++ server_filereader.c
@@ -0,0 +1,42 @@
+#include "server_filereader.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+
+// Function for reading file
+FILE_CONTENT read_file_content(char * file_name, int * error)
+{
+    FILE *pFile;
+
+    size_t result;
+
+    FILE_CONTENT ans;
+
+    // Opening file
+    pFile = fopen(file_name, "r");
+    if (pFile == NULL) {
+        *error = 1;
+  return ans;
+    }
+
+    fseek(pFile, 0, SEEK_END);
+    ans.size = ftell(pFile);
+    rewind(pFile);
+
+    ans.content = (char*)calloc(ans.size, sizeof(char));
+    if (ans.content == NULL) {
+        *error = 2;
+	return ans;
+    }
+
+    result = fread(ans.content, 1, ans.size, pFile);
+    if (result != ans.size) {
+        *error = 3;
+	return ans;  
+    }
+
+    fclose(pFile);
+
+    *error = 0;
+    return ans;
+}
