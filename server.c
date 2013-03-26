bf2cc990d6264b881161ddf0f35d73c7470a0bc3
AnastasiyaKoloskova <anastasiya.koloskova@gmail.com>
2013-03-26 17:46:18 +0300

    Update server.c
    new commit

--- server.c
+++ server.c
@@ -11,21 +11,31 @@
 #include "file_read.h"
 #include "file_sender.h"
 
-typedef struct socketaddress_in SOCKET_ADDRESS, *PSOCKET_ADDRESS;
+typedef struct sockaddr_in SOCKET_ADDRESS, *PSOCKET_ADDRESS;
 typedef struct my_message MESSAGE, *PMESSAGE;
 typedef struct file_content FILE_CONTENT, *PFILE_CONTENT;
 
 int main(int argc, char *argv[])
 {
+	int errorCode = 0;
+	
   if(argc != 2) {
 		printf("Enter the command line parameter with file name.\n");
 		return 1;
 	}
-	file_content information = read_file(argv[1]); 
+	
+	
+	FILE_CONTENT information = read_file_content(argv[1], &errorCode); 
+	
+	if (errorCode!=0){
+		printf("Error %d occured while reading file. Terminating...\n");
+		return 1;
+	}
+	
 	printf("%s\n", information.content);
 	// Defining and initializing socket
 	int my_socket = 0, connfd = 0;
-	socketaddress_in server_address; 
+	SOCKET_ADDRESS server_address; 
 
 	my_socket = socket(AF_INET, SOCK_STREAM, 0);
 	memset(&server_address, 0, sizeof(server_address));
@@ -36,24 +46,23 @@ int main(int argc, char *argv[])
 
 	// Bind socket to address
 	bind(my_socket, (SOCKET_ADDRESS*)&server_address, sizeof(server_address)); 
-
-	int count = 5;
+	init_routine();
+	
 	// Starting server
-	while(count--) {
+	while(1) {
 		// Start listening socket
 		listen(my_socket, 10);
 
 		//Initializing data for starting sending file in the separate thread
-		my_message message;
+		MESSAGE message;
 		message.file = information.content;
 		message.size = information.size;
 
 		// Accepting connection and getting connection identifier
 		message.index = accept(my_socket, (SOCKET_ADDRESS*)NULL, NULL); 
-		pthread_t my_thread;
-		pthread_create(&my_thread, NULL, send_file_to_client, (void*)&message);
+		send_file_to_client(&message);
 	}
-
+	completion_routine();
 
 	close(my_socket);
 	free(information.content);
